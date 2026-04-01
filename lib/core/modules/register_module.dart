f6dcf1de3c4dad6d0747ffabddf171defab430b6
MALAK0244 <malak.hussein0244@icloud.com>
2026-04-01 06:53:42 +0200

    chore: exclude injectable modules from code coverage

--- lib/core/modules/register_module.dart
+++ lib/core/modules/register_module.dart
@@ -2,6 +2,8 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:injectable/injectable.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+// coverage:ignore-file
+
 @module
 abstract class RegisterModule {
   @preResolve
