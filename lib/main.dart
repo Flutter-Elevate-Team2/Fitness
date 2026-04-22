76428794101114f7aef2c91d86918e7d802c0645
AhmedHussien249 <ahmed.hussien950.ah@gmail.com>
2026-04-22 11:39:25 +0200

    feat: refactor shared scaffold, main app configuration, and custom bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+
 import 'package:fitness_app/core/app_router/app_router.dart';
 import 'package:fitness_app/core/controller/session_controller.dart';
 import 'package:fitness_app/core/controller/session_expired.dart';
@@ -8,11 +9,13 @@ import 'package:fitness_app/core/l10n/app_localizations.dart';
 import 'package:fitness_app/core/theming/app_theming.dart';
 import 'package:fitness_app/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 Future<void> main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await dotenv.load(fileName: ".env");
 
@@ -40,21 +43,25 @@ class _MyAppState extends State<MyApp> {
     _subscriptions.add(
       _sessionController.onSessionExpired.listen((_) {
         SessionExpiredHandler.handle();
-      })
+      }),
     );
 
     _subscriptions.add(
       _sessionController.onLogout.listen((_) {
         AppRouter.router.goNamed(Routes.onBoardingName);
-      })
+      }),
     );
   }
+
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
 
     if (!_isSplashRemoved) {
-      precacheImage(AssetImage(Assets.images.authBackground.path), context).then((_) {
+      precacheImage(
+        AssetImage(Assets.images.authBackground.path),
+        context,
+      ).then((_) {
         FlutterNativeSplash.remove();
         _isSplashRemoved = true;
       });
